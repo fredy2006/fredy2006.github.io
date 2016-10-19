4d1eae5dc5a21d82e13de69b0d953f8a8637a20f
Паращевин Константин <opendom@ukr.net>
2016-10-19 09:05:02 +0300

    fix bag script2

--- goitHW/nrprop/js/script2.js
+++ goitHW/nrprop/js/script2.js
@@ -27,6 +27,34 @@ $('.menu ul li').hover(
     }
 );
 
+// begin
+var largeImg = document.getElementById('largeImg');
+var single_image = document.getElementById('single_image');
 
+// console.log('largeImg',largeImg);
+document.getElementById('thumbs').onclick = function(e) {
+  var target = e.target;
+
+  while (target != this) {
+
+    if (target.nodeName == 'A') {
+      showThumbnail(target.href, target.title);
+      return false;
+    }
+
+    target = target.parentNode;
+  }
+
+}
+
+function showThumbnail(href, title) {
+
+  largeImg.src = href;
+  largeImg.alt = title;
+  single_image.href = href.replace("400", "600");
+}
+
+
+// end
 
 })
