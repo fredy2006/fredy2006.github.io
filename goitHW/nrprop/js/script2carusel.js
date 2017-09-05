5ea245653ebc633ec9d9baaf0a1d29bb41926670
Паращевин Константин <opendom@ukr.net>
2017-09-06 00:07:47 +0300

    new js files

---
+++ goitHW/nrprop/js/script2carusel.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+
+console.log('Hello');
+// $("a.grouped_elements").fancybox();
+// $("a#single_image").fancybox();
+// $('.fancybox').fancybox();
+
+// $(".fancybox").fancybox();
+$('#slideshowHolder').jqFancyTransitions({ width: 900, height: 150, delay: 2000, effect: 'zipper', strips: 50 });
+$("a#single_image").fancybox({
+    'transitionIn'  :   'elastic',
+    'transitionOut' :   'elastic',
+    'speedIn'       :   100,
+    'speedOut'      :   200,
+    'overlayShow'   :   false,
+    'padding'       :   0,
+});
+$('.menu ul li').hover(
+    function () {
+      clearTimeout($.data(this,'timer'));
+      $('ul:first', this).slideDown(1200);
+    },
+    function () {
+      $.data(this,'timer', setTimeout($.proxy(function() {
+        $('ul',this).stop(true,true).slideUp(500);
+      }, this), 100));
+    }
+);
+
+// begin
+var largeImg = document.getElementById('largeImg');
+var single_image = document.getElementById('single_image');
+
+// console.log('largeImg',largeImg);
+document.getElementById('thumbs').onclick = function(e) {
+  var target = e.target;
+
+  while (target != this) {
+
+    if (target.nodeName == 'A') {
+      showThumbnail(target.href, target.title);
+      return false;
+    }
+
+    target = target.parentNode;
+  }
+
+}
+
+function showThumbnail(href, title) {
+
+  largeImg.src = href;
+  largeImg.alt = title;
+  single_image.href = href.replace("400", "600");
+}
+
+
+// end
+
+})
---
+++ goitHW/nrprop/js/scriptcarusel.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+console.log('Hello');
+// $("a.grouped_elements").fancybox();
+// $("a#single_image").fancybox();
+// $('.fancybox').fancybox();
+
+// $(".fancybox").fancybox();
+$('#slideshowHolder').jqFancyTransitions({ width: 900, height: 150, delay: 2000, effect: 'zipper', strips: 50 });
+$("a#single_image").fancybox({
+    'transitionIn'  :   'elastic',
+    'transitionOut' :   'elastic',
+    'speedIn'       :   100,
+    'speedOut'      :   200,
+    'overlayShow'   :   false,
+    'padding'       :   0,
+});
+// $("a#single_image").fancybox();
+// перехватывание клика
+// $('a').on('click', function(e){
+//   e.preventDefault();
+// });
+// конец перехватывания клика
+
+$('.menu ul li').hover(
+    function () {
+      clearTimeout($.data(this,'timer'));
+      $('ul:first', this).slideDown(1200);
+    },
+    function () {
+      $.data(this,'timer', setTimeout($.proxy(function() {
+        $('ul',this).stop(true,true).slideUp(500);
+      }, this), 100));
+    }
+);
+
+// Reload img from preview
+// var largeImg = document.getElementById('single_1');
+var largeImg = document.getElementById('largeImg');
+console.log('largeImg',largeImg);
+document.getElementById('thumbs').onclick = function(e) {
+  var target = e.target;
+
+  while (target != this) {
+
+    if (target.nodeName == 'A') {
+      showThumbnail(target.href, target.title);
+      return false;
+    }
+
+    target = target.parentNode;
+  }
+
+}
+
+function showThumbnail(href, title) {
+
+  largeImg.src = href;
+  largeImg.alt = title;
+}
+
+})
